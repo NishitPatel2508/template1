921e88fb0270fbbfc64bd75de5b77c17a6027b7b
NishitPatel2508 <128670053+NishitPatel2508@users.noreply.github.com>
2024-02-05 16:45:33 +0530

    Files Added

---
+++ JS/scrollscript.js
@@ -0,0 +1,36 @@
+// Need to be change is header
+const header = document.querySelector("header");
+const ft = document.querySelector(".fixed-top");
+
+// console.log(header)
+const sectionOne = document.querySelector(".home-intro");
+console.log(sectionOne);
+const sectionOneOptions = {
+    rootMargin:"-600px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(
+  function(entries, sectionOneObserver) {
+    entries.forEach((entry) => {
+        if(!entry.isIntersecting){
+            header.classList.add("nav-scrolled")   
+            
+            ft.classList.add("fixed-top")        
+            ft.classList.add("black")   
+
+        } else{
+            header.classList.remove("nav-scrolled")
+  
+            ft.classList.remove("fixed-top")
+            ft.classList.add("fixed-top")
+            ft.classList.remove("black")  
+        }
+    });
+  },
+  // Pass sectionOneOptions as the second argument
+  sectionOneOptions);
+sectionOneObserver.observe(sectionOne);
+
+
+
+
